1a923d48b89ab387951581b08af6e5a8c59140c4
Kubayev9765 <ShamanRamilov@gmail.com>
2025-03-06 04:52:10 +0500

    Update homework.sql

--- lesson-1/homework.sql
+++ lesson-1/homework.sql
@@ -73,4 +73,78 @@ from Salesman
 where City='Paris'
 
 
+10. Customers with Grade 200
+create table Customer(
+					Customer_ID int,
+					Cust_Name varchar(20),
+					City varchar(20),
+					Grade int,
+					Salesman_ID int
+					)
+
+insert into Customer values(3002, 'Nick Rimando', 'New York', 100, 5001)
+insert into Customer values(3007, 'Brad Davis', 'New York', 200, 5001)
+insert into Customer values(3005, 'Graham Zusi', 'California', 200, 5002)
+insert into Customer values(3008, 'Julian Green', 'London', 300, 5002)
+insert into Customer values(3004, 'Fabian Johnson', 'Paris', 300, 5006)
+insert into Customer values(3009, 'Geoff Cameron', 'Berlin', 100, 5003)
+insert into Customer values(3003, 'Jozy Altidor', 'Moskow', 200, 5007)
+insert into Customer values(3001, 'Brad Guzan', 'London', NULL  , 5005)
+
+
+
+select Customer_ID, Cust_Name,City,Grade,Salesman_ID
+from Customer
+where Grade=200
+
+11. Orders by Salesperson 5001
+select ord_no, ord_date, purch_amt
+from Orders
+where Salesman_id=5001
+
+12. Nobel Winner of 1970
+
+	create table Nobel_Win(
+	YEAR INT,
+	SUBJECT VARCHAR(20),
+	WINNER VARCHAR(20),
+	COUNTRY VARCHAR(20),
+	CATEGORY VARCHAR(20)
+	)
+
+
+
+
+insert into Nobel_Win values(1970, 'Physics','Hannes Alfven','Sweden', 'Scientist')
+insert into Nobel_Win values(		1970, 'Physics','Hannes Alfven','Sweden', 'Scientist')
+insert into Nobel_Win values(		1970, 'Physics','Louis Neel', 'France', 'Scientist')
+insert into Nobel_Win values(		1970, 'Chemistry',' Luis Federico Lel','France','Scientist')
+insert into Nobel_Win values(		1970, 'Physiology',' Ulf von Euler',' Sweden',' Scientist')
+insert into Nobel_Win values(		1970, 'Physiology',' Bernard Katz',' Germany ',' Scientist')
+insert into Nobel_Win values(		1970, 'Literature ', 'Aleksandr', ' Solzhenitsyn',  'Russia')
+insert into Nobel_Win values(		1970, 'Economics ',  'Paul Samuelson ', 'USA', 'Economist')
+insert into Nobel_Win values(		1971, 'Physics ',  'Dennis Gabor ', 'Hungary ','Scientist')
+insert into Nobel_Win values(		1971, 'Chemistry ', ' Gerhard Herzberg', ' Germany ','Scientist')
+insert into Nobel_Win values(		1971, 'Peace ',  ' Willy Brandt', 'Germany ', 'Chancellor')
+insert into Nobel_Win values(		1971, 'Literature ', 'Pablo Neruda ','Chile ', 'Linguist')
+insert into Nobel_Win values(		1971, 'Economics','  Simon Kuznets ', 'Russia', 'Economist')
+insert into Nobel_Win values(		1978, 'Peace', ' Anwar al-Sadat  ',  'Egypt ',  'President')
+insert into Nobel_Win values(		1978, 'Peace  ', ' Menachem Begin ', 'Israel', 'Prime Minister')
+insert into Nobel_Win values(		1987, 'Chemistry ','Donald J. Cram ', 'USA  ', 'Scientist')
+insert into Nobel_Win values(		1987, 'Chemistry', 'Jean-Marie Lehn', 'France', 'Scientist')
+insert into Nobel_Win values(		1987, 'Physiology', 'Susumu Tonegawa', 'Japan ', 'Scientist')
+insert into Nobel_Win values(		1994, 'Economics', 'Reinhard Selten', 'Germany','Economist')
+insert into Nobel_Win values(		1994, 'Peace', 'Yitzhak Rabin', 'Israel ', 'Prime Minister')
+insert into Nobel_Win values(		1987, 'Physics', 'Johannes Georg Bednorz', 'Germany ', 'Scientist')
+insert into Nobel_Win values(		1987, 'Literature',' Joseph Brodsky','Russia ','Linguist')
+insert into Nobel_Win values(		1987, 'Economics',' Robert Solow','USA ','Economist')
+insert into Nobel_Win values(		1994, 'Literature',' Kenzaburo Oe ', 'Japan','Linguist')
+
+
+
+select YEAR, SUBJECT, WINNER
+from Nobel_Win
+where YEAR=1970
+
+
 
