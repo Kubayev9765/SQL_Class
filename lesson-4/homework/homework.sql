0c0b2d8f02a0cd942cec8a14b52de5ca0f249432
Kubayev9765 <ShamanRamilov@gmail.com>
2025-03-28 06:02:31 +0500

    Update homework.sql

--- lesson-4/homework/homework.sql
+++ lesson-4/homework/homework.sql
@@ -1,66 +1,92 @@
-1. Salespeople from Paris or Rome
+Write a query to select the top 5 employees from the Employees table.
+select top (5) 
+*from employees
+order by manager_id desc
 
-create table Salesmen(
-	salesman_id int,
-	name varchar(50),
-	city varchar(50),
-	commission decimal(4,2)
-	)
+Use SELECT DISTINCT to select unique ProductName values from the Products table.
+select distinct Name from Products
 
-insert into Salesmen 
-values
-	(5001, 'James Hoog', 'New York', 0.15),
-	(5002, 'Nail Knite', 'Paris', 0.13),
-	(5005, 'Pit Alex', 'London', 0.11),
-	(5006, 'Mc Lyon', 'Paris', 0.14),
-	(5007, 'Paul Adom', 'Rome', 0.13),
-	(5003, 'Lausen Hen', 'San Jose', 0.12)
+Write a query that filters the Products table to show products with Price > 100.
+select 
+*from Products
+where Price>100
 
-select *from Salesmen
-where  city='Paris' or city='Rome
-
-2. Salespeople in Paris or Rome
-	
-select *from Salesmen	
-where city in  ('Paris' , 'Rome' )
-
-3. Salespeople Not in Paris or Rome
-	
-select *from Salesmen
-where city not in  ('Paris' , 'Rome' )
-
-4. Customers with Specific IDs
-
+Write a query to select all CustomerName values that start with 'A' using the LIKE operator.
 select *from Customers
-where customer_id in (3007,3008,3009)
+where CustomerName  like 'A%'
 
-5. Salespeople with Commission 0.12-0.14
-	
-select *from Salesmen
-where commission between 0.12 and  0.14
+Order the results of a Products query by Price in ascending order.
+select *from Products
+order by Price asc
 
-6. Orders Between 500-4000 Excluding Specific Amounts
-select * from  Orders
-where (purch_amt between 500 and 4000) 
-and purch_amt not in( 948.50 , 1983.43)
+Write a query that uses the WHERE clause to filter for employees with Salary >= 5000 and Department = 'HR'.
+select *from Employees
+ where  Salary >= 5000 and Department = 'HR'
 
-7. Salespeople with Names N-O Range
-select *from Salesmen
-where name between 'A' and 'L'
+Use ISNULL to replace NULL values in the Email column with the text "noemail@example.com".
+select isnull(Email, 'noemail@example.com') from Customers
 
-8. Salespeople with Names Not A-M Range
-select *from Salesmen
-where name not in( 'A' , 'M')
-	
-9. Customers with Names Starting with B	
+Write a query that shows all products with Price BETWEEN 50 AND 100.
+select *
+from Products
+where Price BETWEEN 50 AND 100
+
+Use SELECT DISTINCT on two columns (Category and ProductName) in the Products table.
+
+select distinct Category,Name 
+from Products
+where Category= 'Accessories'
+
+Order the results by ProductName in descending order
+select Name from Products
+order by Name desc
+
+
+Write a query to select the top 10 products from the Products table, ordered by Price DESC.
+select top(10) Price from Products
+order by Price desc
+
+Use COALESCE to return the first non-NULL value from FirstName or LastName in the Employees table.
+select last_name, first_name, COALESCE(FirstName,LastName)  from employees
+
+Write a query that selects distinct Category and Price from the Products table.
+select distinct Category, Price  from Products
+where Category='Accessories'
+
+Write a query that filters the Employees table to show employees whose Age is either between 30 and 40 or Department = 'Marketing'.
+select *from employees
+where age =  between 30 and 40  or Department = 'Marketing'
+
+Use OFFSET-FETCH to select rows 11 to 20 from the Employees table, ordered by Salary DESC.
+select *from employees
+order by salary desc
+offset 20  rows fetch next 11 rows only
+
+
+Write a query to display all products with Price <= 1000 and Stock > 50, sorted by Stock in ascending order.
+select *from Products
+where Price <= 1000 and StockQuantity > 50
+order by StockQuantity asc
+
+Write a query that filters the Products table for ProductName values containing the letter 'e' using LIKE.
+select *from Products
+where Name like 'e%'
+
+Use IN operator to filter for employees who work in either 'HR', 'IT', or 'Finance'.
+select *from Employees
+where Department in('HR', 'IT',  'Finance')
+
+Write a query that uses the ANY operator to find employees who earn more than the average salary of all employees.
+select employee_id, first_name, last_name, salary
+from employees
+where salary > any (select AVG(salary) from employees);
+
+
+Use ORDER BY to display a list of customers ordered by City in ascending and PostalCode in descending order.
 select *from Customers
-where cust_name like('B%')
+order by City asc, ZipCode desc
 
-10. Customers with Names Ending with N
+
+Use ORDER BY to display a list of customers ordered by City in ascending and PostalCode in descending order.
 select *from Customers
-where cust_name like('%N')
-
-11. Salespeople Name Starts 'N' with Fourth 'L'
-select *from Salesmen
-where name like ('N__l%')
-	
+order by City asc, ZipCode desc
